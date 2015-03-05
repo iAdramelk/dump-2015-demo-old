1cd0912b681e5d1e3cc7a9b93624060a24a31ceb
Alexey Ivanov <stupidlogin@gmail.com>
2015-03-06 02:51:04 +0500

    Static version

---
+++ static/postcss/vars/colors.js
@@ -0,0 +1,10 @@
+module.exports = {
+    link: '#00e',
+    hover: '#c00',
+    active: '#c00',
+    visited: '#00e',
+    text: '#333',
+    comment: '#808080',
+    background: '#ddd',
+    borders: '#808080'
+};
